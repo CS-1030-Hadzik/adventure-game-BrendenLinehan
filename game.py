b980d4b2f729641aa1039d025ceb7605e9b45e18
Brenden Linehan <brendenlinehan@weber.mail.edu>
2026-01-19 17:51:22 -0700

    ADD: docstringfor author information

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
+"""
+Adventure Game
+Author: Brenden Linehan
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+"""
+
+# welcome message and introduction
+
 print("Welcome to the Adventure Game!")
 print("Your journey begins here...")
